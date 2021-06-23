9e4ff656b00b6a936504a029d558fc92bf074dca
Eric Wakeman <erwakeman@gmail.com>
2021-06-23 11:48:44 -0600

    change desc to rewareds

--- hogout/server/controllers/AttemptsController.js
+++ hogout/server/controllers/AttemptsController.js
@@ -10,6 +10,25 @@ export class AttemptsController extends BaseController {
       .get('/account/:id/attempts', this.getAttemptsByCreatorId)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('/challenges/:id/attempts', this.createAttempt)
-      .
+      .delete('/attempts/:id', this.deleteAttempt)
+  }
+  async getAllAttempts(req, res, next) {
+    try {
+      const attempts = await 
+    } catch (error) {
+      next(error)
+    }
+  }
+  getAttemptsByChallengeId(arg0, getAttemptsByChallengeId) {
+    throw new Error("Method not implemented.")
+  }
+  getAttemptsByCreatorId(arg0, getAttemptsByCreatorId) {
+    throw new Error("Method not implemented.")
+  }
+  createAttempt(arg0, createAttempt) {
+    throw new Error("Method not implemented.")
+  }
+  deleteAttempt(arg0, deleteAttempt) {
+    throw new Error("Method not implemented.")
   }
 }
--- hogout/server/models/Challenges.js
+++ hogout/server/models/Challenges.js
@@ -5,7 +5,8 @@ export const ChallengesSchema = new Schema(
   {
     creatorId: { type: mongoose.Types.ObjectId, ref: 'Account', required: true },
     name: { type: String, required: true },
-    description: { type: String, required: true },
+    restaurant: { type: String, required: true },
+    rewards: { type: String, required: true },
     rules: { type: String, required: true },
     location: { type: String, required: true },
     image: { type: String },
