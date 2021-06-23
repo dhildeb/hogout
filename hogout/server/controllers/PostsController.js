0f631062c212d1201b6c0b4dc2e4761c85a405cf
Eric Wakeman <erwakeman@gmail.com>
2021-06-23 11:07:53 -0600

    challenges update

---
+++ hogout/server/controllers/PostsController.js
@@ -0,0 +1,9 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import BaseController from '../utils/BaseController'
+
+export class PostsController extends BaseController {
+  constructor() {
+    super('api')
+    this.router
+  }
+}
--- hogout/server/models/Challenges.js
+++ hogout/server/models/Challenges.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema
 export const ChallengesSchema = new Schema(
   {
     creatorId: { type: mongoose.Types.ObjectId, ref: 'Account', required: true },
-    body: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    rules: { type: String, required: true },
     location: { type: String, required: true },
     image: { type: String }
 
