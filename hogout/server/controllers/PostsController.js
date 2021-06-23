c180b0ecd4d2ebc647b885f0c89a763bb6ce2178
Eric Wakeman <erwakeman@gmail.com>
2021-06-23 11:32:55 -0600

    Posts Routes and Methods

--- hogout/server/controllers/PostsController.js
+++ hogout/server/controllers/PostsController.js
@@ -1,9 +1,53 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
+import { postsService } from '../services/PostsService'
 import BaseController from '../utils/BaseController'
 
 export class PostsController extends BaseController {
   constructor() {
-    super('/api')
+    super('api')
     this.router
+      .get('/boards/:id/posts', this.getAllPosts)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('/posts', this.createPost)
+      .put('/posts/:id', this.editPost)
+      .delete('/posts/:id', this.deletePost)
+  }
+
+  async getAllPosts(req, res, next) {
+    try {
+      const posts = await postsService.getAllPosts(req.params.id)
+      res.send(posts)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createPost(req, res, next) {
+    try {
+      const newPost = req.body
+      newPost.creatorId = req.userInfo.id
+      const post = await postsService.createPost(newPost)
+      res.send(post)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editPost(req, res, next) {
+    try {
+      const post = await postsService.editPost(req.params.id, req.userInfo.id, req.body)
+      res.send(post)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async deletePost(req, res, next) {
+    try {
+      const post = await postsService.deletePost(req.params.id, req.userInfo.id)
+      res.send(post)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- hogout/server/services/PostsService.js
+++ hogout/server/services/PostsService.js
@@ -1,5 +1,34 @@
-class PostsService {
+import { dbContext } from '../db/DbContext'
+import { BadRequest } from '../utils/Errors'
 
+class PostsService {
+  async getAllPosts(id) {
+    const posts = await dbContext.Posts.find({ challengeId: id })
+    return posts
+  }
+
+  async createPost(newPost) {
+    const post = await dbContext.Posts.create(newPost)
+    return post
+  }
+
+  async editPost(id, userId, body) {
+    const post = await dbContext.Posts.find({ _id: id, creatorId: userId })
+    if (!post) {
+      throw new BadRequest('You do not have permission to edit this post.')
+    }
+    const editedPost = await dbContext.Posts.findByIdAndUpdate(id, body, { new: true })
+    return editedPost
+  }
+
+  async deletePost(id, userId) {
+    const post = await dbContext.Posts.find({ _id: id, creatorId: userId })
+    if (!post) {
+      throw new BadRequest('You do not have permission to delete this post.')
+    }
+    const deletedPost = await dbContext.Posts.findByIdAndDelete(id)
+    return deletedPost
+  }
 }
 
 export const postsService = new PostsService()
