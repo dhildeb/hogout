481af6b9667f6f1990620d6e62c17185cb87bac1
Eric Wakeman <erwakeman@gmail.com>
2021-06-24 10:08:35 -0600

    difficulty front end functions

--- hogout.client/src/services/AttemptsService.js
+++ hogout.client/src/services/AttemptsService.js
@@ -7,6 +7,17 @@ class AttemptsService {
     const res = await api.get('api/attempts')
     AppState.attempts = res.data.map(a => new Attempt(a))
   }
+
+  async getAttemptsByCreatorId(id) {
+    const res = await api.get('api/account/' + id + '/attempts')
+    AppState.attempts = res.data.map(a => new Attempt(a))
+  }
+
+  async createAttempt(challengeId, newAttempt) {
+    const res = await api.post('api/challenges' + challengeId + '/attempts', newAttempt)
+
+    AppState.attempts.push(new Attempt(res.data))
+  }
 }
 
 export const attemptsService = new AttemptsService()
--- hogout.client/src/services/RatingsService.js
+++ hogout.client/src/services/RatingsService.js
@@ -12,6 +12,18 @@ class RatingsService {
     const res = await api.get('api/challenges/' + id + '/difficulty')
     AppState.difficultyRatings = res.data.map(r => new Rating(r))
   }
+
+  async handleDifficultyRating(challengeId, newRating) {
+    const res = await api.post('api/challenges/' + challengeId + '/difficulty', newRating)
+    const rating = AppState.difficultyRatings.find(r => r.id === res.data.id)
+
+    if (!rating) {
+      AppState.difficultyRatings.push(new Rating(res.data))
+    } else {
+      AppState.difficultyRatings = AppState.difficultyRatings.filter(r => r.id !== res.data.id)
+      AppState.difficultyRatings.push(new Rating(res.data))
+    }
+  }
 }
 
 export const ratingsService = new RatingsService()
