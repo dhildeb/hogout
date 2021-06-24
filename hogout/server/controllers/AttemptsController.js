97f03e18d2593d7ec008132661bc8405bf46122d
Eric Wakeman <erwakeman@gmail.com>
2021-06-24 10:36:49 -0600

    push

--- hogout/server/controllers/AttemptsController.js
+++ hogout/server/controllers/AttemptsController.js
@@ -9,8 +9,6 @@ export class AttemptsController extends BaseController {
       .get('', this.getAllAttempts)
       .get('/account/:id/attempts', this.getAttemptsByCreatorId)
       .use('', Auth0Provider.getAuthorizedUserInfo)
-      .post('/challenges/:id/attempts', this.createAttempt)
-      .delete('/challenges/:id/attempts', this.deleteAttempt)
   }
 
   async getAllAttempts(req, res, next) {
@@ -26,27 +24,6 @@ export class AttemptsController extends BaseController {
     try {
       const attempts = await attemptsService.getAttemptsByCreatorId(req.params.id)
       res.send(attempts)
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async createAttempt(req, res, next) {
-    try {
-      const newAttempt = req.body
-      newAttempt.creatorId = req.userInfo.id
-      newAttempt.challengeId = req.params.id
-      const attempt = await attemptsService.createAttempt(newAttempt)
-      res.send(attempt)
-    } catch (error) {
-      next(error)
-    }
-  }
-
-  async deleteAttempt(req, res, next) {
-    try {
-      const attempt = await attemptsService.deleteAttempt(req.params.id, req.userInfo.id)
-      res.send(attempt)
     } catch (error) {
       next(error)
     }
