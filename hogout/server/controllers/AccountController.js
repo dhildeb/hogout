a3eb41a766ba1cde962ed2bf65e88245963de995
Eric Wakeman <erwakeman@gmail.com>
2021-06-24 10:27:49 -0600

    get challenge by user id

--- hogout/server/controllers/AccountController.js
+++ hogout/server/controllers/AccountController.js
@@ -1,5 +1,6 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
+import { challengesService } from '../services/ChallengesService'
 import BaseController from '../utils/BaseController'
 
 export class AccountController extends BaseController {
@@ -7,9 +8,19 @@ export class AccountController extends BaseController {
     super('account')
     this.router
       .get('/:id', this.getProfile)
+      .get('/:id/challenges', this.getChallengesByUserId)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
       .put('', this.editUserAccount)
+  }
+
+  async getChallengesByUserId(req, res, next) {
+    try {
+      const challenges = await challengesService.getChallengesByUserId(req.params.id)
+      res.send(challenges)
+    } catch (error) {
+      next(error)
+    }
   }
 
   async getProfile(req, res, next) {
