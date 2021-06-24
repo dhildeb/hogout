98dae7617b07ed7ab8e6d019be680632721dbbbf
Eric Wakeman <erwakeman@gmail.com>
2021-06-24 10:19:08 -0600

    Review ratings funvtions

--- hogout.client/src/services/RatingsService.js
+++ hogout.client/src/services/RatingsService.js
@@ -24,6 +24,28 @@ class RatingsService {
       AppState.difficultyRatings.push(new Rating(res.data))
     }
   }
+
+  async getReviewRatings() {
+    const res = await api.get('api/review')
+    AppState.reviewRatings = res.data.map(r => new Rating(r))
+  }
+
+  async getReviewRatingsByChallengeId(id) {
+    const res = await api.get('api/challenges/' + id + '/review')
+    AppState.reviewRatings = res.data.map(r => new Rating(r))
+  }
+
+  async handleReviewRating(challengeId, newRating) {
+    const res = await api.post('api/challenges/' + challengeId + '/review', newRating)
+    const rating = AppState.reviewRatings.find(r => r.id === res.data.id)
+
+    if (!rating) {
+      AppState.reviewRatings.push(new Rating(res.data))
+    } else {
+      AppState.reviewRatings = AppState.reviewRatings.filter(r => r.id !== res.data.id)
+      AppState.reviewRatings.push(new Rating(res.data))
+    }
+  }
 }
 
 export const ratingsService = new RatingsService()
