5ea99f47c0d0974661d3a507edaf84b2721ecbc3
Eric Wakeman <erwakeman@gmail.com>
2021-06-24 09:09:23 -0600

    challenge service

--- hogout.client/src/services/ChallengesService.js
+++ hogout.client/src/services/ChallengesService.js
@@ -1,18 +1,17 @@
 import { AppState } from '../AppState'
+import { Challenge } from '../models/Challenge'
 import { api } from './AxiosService'
 
 class ChallengesService {
   async getAllChallenges() {
     const res = await api.get('api/challenges')
-    AppState.challenges = res.data
+    AppState.challenges = res.data.map(c => new Challenge(c))
   }
 
   async getChallengeById(id) {
     const res = await api.get('api/challenges/' + id)
-    AppState.activeChallenge = res.data
+    AppState.activeChallenge = new Challenge(res.data)
   }
-
-  async 
 }
 
 export const challengesService = new ChallengesService()
