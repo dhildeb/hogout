8fd945325f1a300bd431af2d71cecfa65d997d9e
Jared VandeWater <jaredvande@gmail.com>
2021-06-25 13:58:55 -0600

    post switcher

--- hogout.client/src/components/Post.vue
+++ hogout.client/src/components/Post.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="row ">
     <div class="col-12 d-flex justify-content-center">
-      <img class="pic-size" :src="post.image1" :alt="post.id" v-if="post.image1">
-      <!-- <img class="pic-size" :src="post.image2" :alt="post.id" v-if="post.image2">
-      <img class="pic-size" :src="post.image3" :alt="post.id" v-if="post.image3"> -->
+      <img class="pic-size" :src="post.image1" :alt="post.id" v-if="state.currentPic === 1 && post.image1">
+      <img class="pic-size" :src="post.image2" :alt="post.id" v-if="state.currentPic === 2 && post.image2">
+      <img class="pic-size" :src="post.image3" :alt="post.id" v-if="state.currentPic === 3 && post.image3">
+    </div>
+  </div>
+  <div class="row justify-content-between">
+    <div class="col">
+      <button @click="changePic('next')" class="btn btn-primary">
+        <i class="mdi mdi-arrow-left-thick"></i>
+      </button>
+    </div>
+    <div class="col d-flex justify-content-end">
+      <button @click="changePic('prev')" class="btn btn-primary">
+        <i class="mdi mdi-arrow-right-thick"></i>
+      </button>
     </div>
   </div>
   <div class="row">
@@ -23,12 +35,26 @@ export default {
   props: { post: { type: Object, required: true } },
   setup(props) {
     const state = reactive({
-      currentPic: 0
+      currentPic: 1
     })
     return {
       state,
       loadProfile() {
         router.push({ path: `/profile/${props.post.creatorId}` })
+      },
+      changePic(direction) {
+        if (direction === 'prev') {
+          state.currentPic--
+        }
+        if (direction === 'next') {
+          state.currentPic++
+        }
+        if (state.currentPic > 3) {
+          state.currentPic = 1
+        }
+        if (state.currentPic < 1) {
+          state.currentPic = 3
+        }
       }
     }
   }
