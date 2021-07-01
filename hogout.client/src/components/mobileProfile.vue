f805e7fcb3c05c8fbeb29869753a18eb1998f564
Jared VandeWater <jaredvande@gmail.com>
2021-06-30 20:09:52 -0600

    burger

--- hogout.client/src/components/mobileProfile.vue
+++ hogout.client/src/components/mobileProfile.vue
@@ -5,23 +5,23 @@
     </a>
     <ul>
       <li>
-        <p class="btn-floating bunbottom" @click="state.pictureForm = !state.pictureForm">
+        <p class="btn-floating bunbottom otext" @click="state.pictureForm = !state.pictureForm">
           Pics
         </p>
       </li>
 
       <li>
-        <p class="btn-floating meat" @click="state.locationForm = !state.locationForm">
+        <p class="btn-floating meat otext" @click="state.locationForm = !state.locationForm">
           Loc
         </p>
       </li>
       <li>
-        <p class="btn-floating cheese" @click="state.bioForm = !state.bioForm">
+        <p class="btn-floating cheese otext" @click="state.bioForm = !state.bioForm">
           Bio
         </p>
       </li>
       <li>
-        <p class="btn-floating lettuce" @click="state.nameForm = !state.nameForm" name="edit name">
+        <p class="btn-floating lettuce otext" @click="state.nameForm = !state.nameForm" name="edit name">
           Name
         </p>
       </li>
@@ -211,16 +211,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.text-shdw{
+
+.otext{
+  font-size: 1.5rem;
+  padding-top: 1.5rem;
+text-shadow: rgb(0, 0, 0) 2px 0px 0px, rgb(0, 0, 0) 1.75517px 0.958851px 0px, rgb(0, 0, 0) 1.0806px 1.68294px 0px, rgb(0, 0, 0) 0.141474px 1.99499px 0px, rgb(0, 0, 0) -0.832294px 1.81859px 0px, rgb(0, 0, 0) -1.60229px 1.19694px 0px, rgb(0, 0, 0) -1.97998px 0.28224px 0px, rgb(0, 0, 0) -1.87291px -0.701566px 0px, rgb(0, 0, 0) -1.30729px -1.5136px 0px, rgb(0, 0, 0) -0.421592px -1.95506px 0px, rgb(0, 0, 0) 0.567324px -1.91785px 0px, rgb(0, 0, 0) 1.41734px -1.41108px 0px, rgb(0, 0, 0) 1.92034px -0.558831px 0px;}
+
+li{
+  margin: 0;
+  margin-bottom: 0;
+  align-self: flex-start;
 
 }
 .fullburger{
+   width: 80px;
+  height:80px;
     background-image: url('../assets/img/b-full.png');
    background-size: contain;
    background-repeat: no-repeat;
 }
 
 .buntop{
+  width: 80px;
+  height:80px;
   background-image: url('../assets/img/b-buntop.png');
    background-size: contain;
    background-repeat: no-repeat;
@@ -292,8 +305,9 @@ background-color: transparent;
 box-shadow: none;
 border-radius: 0;
 margin: 0;
-width: 56px;
-height: 56px;
+width: 80px;
+height: 80px;
+left: 0px;
 transition: all .3s;
 }
 
@@ -302,7 +316,6 @@ transition: all .3s;
 background-color: transparent;
 box-shadow: none;
 margin: 0;
-transition: all .3s;
 top:5rem;
 left:1rem;
 right: 90rem;
@@ -310,8 +323,8 @@ bottom: 56rem;
 
   ul{
   top:4.5rem;
-  left:0;
-  margin:0;
+  left:0px;
+
   }
 }
 
