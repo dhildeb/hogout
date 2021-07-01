4c0da0d9a0469f668e400f8c4241ec87f6203f9e
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-07-01 16:45:28 -0600

    scroll-bar fix

--- hogout.client/src/App.vue
+++ hogout.client/src/App.vue
@@ -22,26 +22,27 @@ export default {
 <style lang="scss">
 @import "./assets/scss/main.scss";
 ::-webkit-scrollbar {
-  height: 50vh;
-  width: 20px;;
+  width: 2rem;
 }
  ::-webkit-scrollbar-track {
    height: 40vh;
-   background-color: transparent;
+   background-color: #36544a;
    opacity: 0;
  }
 ::-webkit-scrollbar-thumb {
-background-image: url('https://res.cloudinary.com/ddbxzcb7k/image/upload/v1564751401/fill-01_vgkaj7.svg');
+  background-image: url('https://res.cloudinary.com/ddbxzcb7k/image/upload/v1564751401/fill-01_vgkaj7.svg');
 }
 ::-webkit-scrollbar-button{
-height: 16px;
+  height: 16px;
 width: 16px;
 }
 ::-webkit-scrollbar-button:vertical:decrement{
+  background-color: #36544a;
   background-image: url('./assets/img/b-buntop.png');
   background-size: cover;
 }
 ::-webkit-scrollbar-button:vertical:increment{
+  background-color: #36544a;
   background-image: url('./assets/img/b-bunbottom.png');
   background-size: cover;
 }
