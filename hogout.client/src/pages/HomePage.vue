fe41087136615e95a1465367d512282b203cb39c
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-07-02 14:36:13 -0600

    small fix

--- hogout.client/src/pages/HomePage.vue
+++ hogout.client/src/pages/HomePage.vue
@@ -57,7 +57,7 @@
                aria-expanded="false"
                title="Filter By Difficulty"
           >
-            {{ state.state }}
+            {{ state.newState }}
           </div>
           <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
             <a class="dropdown-item" @click="filterState('Idaho')">Idaho</a>
@@ -94,7 +94,7 @@ export default {
       account: computed(() => AppState.account),
       challenges: computed(() => AppState.challenges),
       temp: computed(() => AppState.tempChallenges),
-      state: 'State',
+      newState: 'State',
       difficulty: 'Difficulty',
       rating: 'Rating',
       filterBy: {}
